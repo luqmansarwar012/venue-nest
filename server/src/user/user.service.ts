7d938c33c9fd3b60eafeddedc4d712ab18e4f6c3
Raja Luqman Sarwar <luqmanrajput012@gmail.com>
2025-01-13 18:14:09 +0500

    feat(user): inject user model in user service

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UpdateUserDto } from './dto/updateUser.dto';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './entities/user.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UserService {
+  private logger: Logger = new Logger(UserService.name);
+
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
   create(createUserDto: CreateUserDto) {
     console.log(createUserDto);
     return 'This action adds a new user';
